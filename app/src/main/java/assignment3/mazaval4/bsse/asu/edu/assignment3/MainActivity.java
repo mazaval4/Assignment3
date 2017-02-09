2c62a7ecd34bef083455072bbcfc9c2ed347e92b
Miguel Zavala (Student) <mazaval4@sundevils.asu.edu>
2017-02-09 03:15:03 -0700

    aofas

--- app/src/main/java/assignment3/mazaval4/bsse/asu/edu/assignment3/MainActivity.java
+++ app/src/main/java/assignment3/mazaval4/bsse/asu/edu/assignment3/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -22,18 +24,18 @@ Place class creates itself from the json file.
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private GridLayoutManager gridLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        PlaceLibrary pl = PlaceLibrary.getInstance();
+        PlaceLibrary library = PlaceLibrary.getInstance();
 
-        pl.fromJson(getAssets());
-
-
-
-
+        library.fromJson(getAssets());
+        gridLayoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(gridLayoutManager);
     }
 }
